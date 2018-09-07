f4e5fbf87fed04f846c0a2a3557bb348b5df317d
Szczebrzeszyn <krystian.p20@interia.pl>
2018-09-07 14:19:31 +0200

    programowanie i algorytmika

---
+++ klasa 2/python/szablon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+
+def main(args):
+    #a = 0 #inicjacja zmiennej
+    a = input('Podaj liczbę: ')
+    print(a)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ klasa 2/szablon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	
+    int a; //deklaracja zmiennej
+    a = 0; //inicjacja zmiennej
+    cout << "Podaj liczbę: ";
+    cin >> a; //std:: <- standardowa biblioteka
+    cout << a;    
+	return 0;
+}
+
