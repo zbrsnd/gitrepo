e1ec8cdc897c22815c2611ce8cf21ceabeae45d6
Szczebrzeszyn <krystian.p20@interia.pl>
2018-10-24 10:34:58 +0200

    c++ + codeblocks

--- klasa 2/c++/euklides.cpp
+++ klasa 2/c++/euklides.cpp
@@ -6,18 +6,61 @@
 
 #include <iostream>
 
-using namespace std;0
+using namespace std;
+
+int nwd_optymalny(int a, int b)
+{   
+    int licznik = 0;
+    
+    while (a > 0) 
+    {
+        a = a % b;
+        b = b - a;
+        licznik++;
+    }
+    
+    cout << "Liczba powtórzeń: " << licznik << endl;
+    
+    return b;
+}
+
+int nwd_klasyczny(int a, int b)
+{
+    int licznik = 0;
+    
+    while(a!=b)
+	{
+		if (a > b)
+        {
+			a = a - b;
+            licznik++;
+		}
+        else
+        {
+			b = b - a;
+            licznik++;
+        }
+    }
+    
+    cout << "Liczba powtórzeń: " << licznik << endl;
+    
+    return a;
+}
 
 int main(int argc, char **argv)
 {
-	
-    int a, b;
-    cout << "Podaj dwie liczby";
-    cin >> a >> b;
-    int nwd = nwd1(a,b);
-    cout << NWD(
+	int a, b;
+    a = b = 0;
     
-    
+    cout << "Podaj liczbę: ";
+    cin >> a;
+            
+    cout << "Podaj drugą liczbę: ";
+    cin >> b;
+
+    cout << nwd_klasyczny(a, b) << endl;
+    cout << nwd_optymalny(a, b);
+
 	return 0;
 }
 
---
+++ klasa 2/c++/minmax/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+void minmax1() {
+    char dalej = 't';
+    int liczba = 0;
+    int min = 0;
+    while(dalej = 't') {
+        cout << "Podaj liczbę: ";
+    }
+
+
+}
+
+int main()
+{
+    cout << "xd" << endl;
+    return 0;
+}
