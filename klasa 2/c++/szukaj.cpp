b4e5e76226a1ef1ef26c6d3cf115d8b6caea3e06
Szczebrzeszyn <krystian.p20@interia.pl>
2019-01-11 14:19:44 +0100

    szukaj rekurencja

--- klasa 2/c++/szukaj.cpp
+++ klasa 2/c++/szukaj.cpp
@@ -1,10 +1,6 @@
-/*
- * szukaj.cpp
- */
-
-
 #include <iostream>
 using namespace std;
+
 
 void wypelnij_los(int tab[], int n) {
     srand(time(NULL));//inicjacja generatora liczb pseudolosowych
@@ -13,74 +9,98 @@ void wypelnij_los(int tab[], int n) {
     }
 }
 
+
 void drukuj(int tab[], int n) {
     for(int i = 0; i < n; i++) {
         cout << tab[i] << " ";
     }
 }
 
+
 void sort_insert(int tab[], int n) {
-    cout << "\nSortowanie przez wstawianie\n";
     int i, j, tmp;
-    int licznik = 0;
     for (i = 1; i < n; i++) {
         tmp = tab[i];
         j = i - 1;
-        licznik++;
         while (j >= 0 && tab[j] > tmp) {
             tab[j+1] = tab[j];
             j--;
         }
         tab[j+1] = tmp;
     }
-    cout << "\nPowtórzeń: " << licznik << endl;
 }
 
 
-int szukaj_lin(int tab[], int n, int szuk) { // LEPSZY ALGORYTM DZIEL I ZWYCIEZAJ, ten niezbyt wydajny kurła XD
-    
+int szukaj_lin(int tab[], int n, int el_szuk){
 
-    for (int i = 0; i < n; i++) {
-        if (tab[i] == szuk) return i;
+    for (int i = 0; i < n; i++)
+        if (tab[i] == el_szuk)
+            return i;
     return -1;
-      }
+}
+ // dziel i zwyciężaj
+
+int szukaj_bin_lin(int tab[], int n, int el_szuk){
+    int poczatek, koniec, srodek;
+    poczatek = 0;
+    koniec = n - 1;
+
+    while(poczatek <= koniec){
+        srodek = (poczatek + koniec) / 2;
+        if (tab[srodek] == el_szuk){
+            return srodek;
+        } 
+
+        else if(el_szuk < tab[srodek])
+            koniec = srodek - 1;
+        else poczatek = srodek + 1;
     }
+    return -1;
+
+}
+
+int szukaj_bin_rek(int tab[], int el_szuk, int poczatek, int koniec) {
     
-int szukaj_bin_lin(int tab[], int n, int szuk) {
+    if (poczatek <= koniec) {
+        int srodek = (poczatek + koniec) / 2;
+        if (tab[srodek] == el_szuk) return srodek;
+        if (el_szuk < tab[srodek]) 
+            return szukaj_bin_rek(tab, el_szuk, poczatek, srodek-1);
+        else 
+            return szukaj_bin_rek(tab, el_szuk, srodek+1, koniec);
     
-    int p, k, s;
-    p = 0; k = n - 1;
-    
-    
-    while (p <= k) {
-        s = (p+k)/2;
-        if (tab[s] == szuk) {
-            p = s;
-            break;    
-        } else if (szuk < tab[s]) k = s - 1;
-        else p = s + 1;
-    }
         
-     
-    return p;
+        }
+    return -1;
     }
+    
 
 
 int main(int argc, char **argv)
 {
-	int n = 20;
+    int n = 20;
     int tab[n];
     wypelnij_los(tab, n);
-    drukuj(tab, n);
-    int szuk = 0;
-    cout << "Podaj liczbę: "; cin >> szuk;
     sort_insert(tab, n);
-    int indeks = szukaj_bin_lin(tab, n, szuk);
-    if (indeks >= 0) cout << "Znaleziono: " << indeks << endl;
-    else cout << "Nie znaleziono" << endl;
-    
-    //~if (indeks != -1) cout << "Znaleziono";
-    //~else cout << "Nie znaleziono";
-	return 0;
+    drukuj(tab, n);
+    int el_szuk = 0;
+    cout << "Podaj liczbę: ";
+    cin >> el_szuk;
+	int index = 0;
+    //~index = szukaj_bin_rek(tab, el_szuk, 0, n-1);
+    index = szukaj_bin_lin(tab, n, el_szuk);
+    if(index >= 0)cout<<"Znaleziono: " << index << endl;
+    else 
+        cout << "Nie znaleziono";
+
+    //~if (index != -1)
+        //~cout << "Znaleziono";
+    //~else     
+        //~cout << "Nie znaleziono";
+
+    sort_insert(tab, n);
+    szukaj_bin_lin(tab, n, el_szuk);
+
+    return 0;
 }
 
