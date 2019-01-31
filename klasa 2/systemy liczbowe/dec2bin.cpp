5f84066a1a85e4bc87b50a13856adc92fb90e21b
Szczebrzeszyn <krystian.p20@interia.pl>
2019-01-31 07:55:44 +0100

    SYSTEMY LICZBOWE XD

---
+++ klasa 2/systemy liczbowe/dec2bin.cpp
@@ -0,0 +1,30 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void dec2bin(int l, int tab[8]) {
+    
+    for(i = 0; i < 8; i++) {
+        tab[i] = l%2;
+        l = l/2
+        
+        }
+    
+    
+    
+    }
+    
+int main(int argc, char **argv)
+{
+	int tab[8];
+    int l;
+    cout << "Podaj liczbę" << endl;
+    cin >> l;
+    
+	return 0;
+}
+
