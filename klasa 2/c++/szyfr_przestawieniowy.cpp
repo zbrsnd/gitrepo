88d4a9c3788381b3925d256aeb3ad8e4b680e12b
Szczebrzeszyn <krystian.p20@interia.pl>
2019-02-20 10:35:43 +0100

    szyfr cezara + przestawieniowy

--- klasa 2/c++/szyfr_przestawieniowy.cpp
+++ klasa 2/c++/szyfr_przestawieniowy.cpp
@@ -4,6 +4,7 @@
 
 
 #include <iostream>
+#include <string.h> //strlen
 using namespace std;
 
 #define MAKS 100
@@ -18,25 +19,11 @@ using namespace std;
 //~ }
 
 void szyfruj(char tb[], int klucz){
-    klucz = klucz % 26;
-    int i = 0;
-    int kod = 0;
-    while (tb[i] != '\0') {
-        kod = (int)tb[i];
-        if (tb[i] == ' ') {
-            cout << tb[i];
-        } else if (kod < 91) {
-            kod += klucz;
-            if ( kod > 90) kod -= 26;
-        } else {
-            kod += klucz;
-            if (kod > 122) kod -= 26;
-        }
-        cout << (char)kod;
-        tb[i] = (char)kod;
-        i++;
-    }
-    cout << endl;
+    int ile = strlen(tb);
+    //ile znaków uzupełnić kropkami
+    //uzupełnić kropkami
+    //dodac do tb kropki
+    
 }
 
 
