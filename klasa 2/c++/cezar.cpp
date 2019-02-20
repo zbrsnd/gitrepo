a7a99b3f8139d57852f43a1435181cedb3ab653e
zbrsnd <zbrsnd@gmail.com>
2019-02-20 20:17:12 +0100

    szyfr cezara - funkcja deszyfruj

--- klasa 2/c++/cezar.cpp
+++ klasa 2/c++/cezar.cpp
@@ -17,6 +17,29 @@ using namespace std;
     //~ }
 //~ }
 
+void deszyfruj(char tb[], int klucz){
+    klucz = klucz % 26;
+    int kod = 0, i = 0;
+    while (tb[i] != '\0'){
+        kod = (int) tb[i];
+        if (tb[i] == ' ')
+        {
+            ;
+        } else if (kod < 91) {
+            kod -= klucz;
+            if (kod < 65) kod += 26;
+        } else {
+            kod -= klucz;
+            if (kod < 97) kod += 26;
+        }
+
+        cout << (char)kod;
+        tb[i]= (char)kod;
+        i++;
+    }
+    cout << endl;
+}
+
 void szyfruj(char tb[], int klucz){
     klucz = klucz % 26;
     int i = 0;
@@ -50,6 +73,7 @@ int main(int argc, char **argv)
     cout << "Podaj klucz: ";
     cin >> klucz;
     szyfruj(tekst, klucz);
+    deszyfruj(tekst, klucz);
 
     return 0;
 }
